24e7866fc2cdd2e068fd05c4e9d182aad1a8f1bb
KaMeHb-UA <marlock@etlgr.com>
2019-10-04 10:11:26 +0300

    initial

---
+++ pack.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+mkdir -p pack/usr/share/applications
+mkdir -p pack/usr/share/icons
+mkdir -p pack/opt/mfcoin
+
+cp -r dist/bin dist/utxo_snapshot dist/genesis*.dat pack/opt/mfcoin
+cp -r dist/include dist/lib dist/share pack/usr
+
+cd pack/usr/share/man/man1
+mans=$(ls | grep bitcoin)
+
+bittomf() {
+    echo "$1" | sed 's/bitcoin/mfcoin/'
+}
+
+for i in $mans; do
+    mv "$i" $(bittomf "$i")
+done
+
+cd -
+mkdir aur
+git clone https://aur.archlinux.org/mfcoin-bin.git aur
+
+mv aur/start pack/opt/mfcoin
+mv aur/mfcoin.desktop pack/usr/share/applications
+mv aur/mfcoin.svg pack/usr/share/icons
+
+rm -rf aur
+
+cp -r addons/* pack
